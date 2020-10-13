5ccf7a1077a2b134fec93851a41250697effa03e
Simon Laura <simonlaura91@gmail.com>
2020-10-13 14:20:49 +0200

    pároskeresés

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,27 @@ public class Main {
         int[] randomArray = new int[20];
 
         for (int i = 0; i < randomArray.length; i++) {
-            randomArray[i] = (int)(Math.random() * 100);
+            randomArray[i] = (int) (Math.random() * 100);
         }
     }
 
-    public static int getMin(int[] inputArray){
+    public static int getMin(int[] inputArray) {
         int minValue = inputArray[0];
-        for(int i=1;i<inputArray.length;i++){
-            if(inputArray[i] < minValue){
+        for (int i = 1; i < inputArray.length; i++) {
+            if (inputArray[i] < minValue) {
                 minValue = inputArray[i];
             }
         }
         return minValue;
     }
-}
 
+    public static int evenCounter(int[] inputArray) {
+        int count = 0;
+        for (int i = 0; i < inputArray.length; i++) {
+
+            if ((inputArray[i] & 2) == 0)
+                count++;
+        }
+        return count;
+    }
+}
